2472b2794705c03fb3469642d2f1d1f42e5cbc29
claxten10 <claxten10@gmail.com>
2024-08-31 06:34:54 +0200

    gold: Initial skeleton tree
    Signed-off-by: claxten10 <claxten10@gmail.com>

---
+++ README.md
@@ -0,0 +1,9 @@
+# AOSP Device Tree for the Redmi Note 13 5G/13R Pro/POCO X6 Neo 5G (gold)
+
+```
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ lineage_gold.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/xiaomi/gold/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_gold
+PRODUCT_DEVICE := gold
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := 2312FRAFDI
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Redmi/vnd_gold/gold:12/UP1A.231005.007/V816.0.11.0.UNQMIXM:user/release-keys
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gold-user 12 UP1A.231005.007 V816.0.11.0.UNQMIXM release-keys"
